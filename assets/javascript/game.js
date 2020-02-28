38df7a9b991c575a151499f0c3855135ff97e264
Bailee Dastugue <bailee.dast@gmail.com>
2020-02-28 12:05:38 -0600

    checks whether user click is correct or incorrect

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var question1 = {
     fullQuestion: "what's my name",
     answerChoices: ["Bailee", "Jack", "Caroline", "Max"],
-    correctAnswer: 0
+    correctAnswer: "0"
 }
 
 var question2 = {
@@ -13,11 +13,12 @@ var question2 = {
 
 var questionArray = [question1, question2];
 var questionNumber = 0;
+var currentQuestion;
 
 // function that creates the questions + buttons for each question
 function createQuestions () {
     // creates questions based on what the question number is
-    var currentQuestion = questionArray[questionNumber];
+    currentQuestion = questionArray[questionNumber];
     // parses the question to HTML
     $("#full-question").append(currentQuestion.fullQuestion);
     // a loop to go through each question's answer choice array
@@ -32,3 +33,13 @@ function createQuestions () {
 }
 
 createQuestions();
+
+$(".answerChoice").on("click", function(event) {
+    if ($(this).val() === currentQuestion.correctAnswer) {
+        console.log("correct");
+    }
+    else {
+        console.log("incorrect");
+    }
+    
+})
